635be1d84b3e6e4d0e2704645dec9b008fa4283c
Jan Koudijs <theappgineer@gmail.com>
2024-07-04 11:55:05 +0200

    Keep build directory out of bundle archives

--- bundle-linux-x86_64.sh
+++ bundle-linux-x86_64.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
-NAME=build/community_remote-linux-x86_64
+NAME=community_remote-linux-x86_64
 
 flutter clean
 flutter build linux --release
-mkdir -p $NAME/icons
-cp -r flatpak/icons/256x256 flatpak/icons/512x512 $NAME/icons
-cp flatpak/com.theappgineer.community_remote.desktop flatpak/com.theappgineer.community_remote.metainfo.xml $NAME/
-cp -r build/linux/x64/release/bundle/* $NAME/
+mkdir -p build/$NAME/icons
+cp -r flatpak/icons/256x256 flatpak/icons/512x512 build/$NAME/icons
+cp flatpak/com.theappgineer.community_remote.desktop flatpak/com.theappgineer.community_remote.metainfo.xml build/$NAME/
+cp -r build/linux/x64/release/bundle/* build/$NAME/
+cd build/
 tar czf $NAME.tar.gz $NAME
